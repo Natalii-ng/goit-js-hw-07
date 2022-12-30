d877794747bb14e18c0c9705e7e66146ed53d961
natalia <geracimova.ng@gmail.ru>
2022-12-30 13:04:39 +0200

    2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryMarkup(img) {
+    return img
+        .map(({ preview, original, description }) => {
+            return `
+            <li><a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}"/>
+            </a></li>`;
+        })
+        .join('');
+};
+
+  
+    const Lightbox = new SimpleLightbox('.gallery__item', {
+        captionsData: 'alt',
+        captionDelay: 250,
+    });
+
